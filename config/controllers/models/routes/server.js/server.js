5f99f1855783112d61156835f62cf2c9f017a841
Archana8695 <sarchana348@gmail.com>
2022-05-13 18:11:59 +0530

    Checkpoint 7

--- config/controllers/models/routes/server.js/server.js
+++ config/controllers/models/routes/server.js/server.js
@@ -6,6 +6,13 @@ const app = express();
 const movieRouter = require("./routes/movie.routes");
 const genreRouter = require("./routes/genre.routes");
 const artistRouter = require("./routes/artist.routes");
+const userRouter = require("./routes/user.routes");
+const bodyParser = require("body-parser");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 const corsOptions = {
   origin: "http://localhost:3001",
 };
@@ -28,6 +35,7 @@ db.mongoose
     process.exit();
   });
   app.use("/api", movieRouter, genreRouter, artistRouter);
+  app.use("/api/auth", userRouter);
   app.listen(PORT, () => {
     console.log("listening to port number " + PORT);
   });
