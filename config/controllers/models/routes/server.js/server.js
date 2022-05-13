ecd1046e8d6a647aafed43c114f946a7fcfe2560
Archana8695 <sarchana348@gmail.com>
2022-05-13 15:15:53 +0530

    Checkpoint 6- Part 1

--- config/controllers/models/routes/server.js/server.js
+++ config/controllers/models/routes/server.js/server.js
@@ -1,26 +1,16 @@
-const http = require("http");
-const PORT = 9000;
+const express = require("express");
+const PORT = 3000;
 const db = require("./models");
-const app = http.createServer((req, res) => {
-  if (req.url === "/movies") {
-    res.writeHead(200, { contenttype: "text/html" });
-    res.write("All Movies Data in JSON format from Mongo DB");
-    res.end();
-  }
-  if (req.url === "/genres") {
-    res.writeHead(200, { contenttype: "text/html" });
-    res.write("All Genres Data in JSON format from Mongo DB");
-    res.end();
-  }
-  if (req.url === "/artists") {
-    res.writeHead(200, { contenttype: "text/html" });
-    res.write("All Artists Data in JSON format from Mongo DB");
-    res.end();
-  }
-});
+const cors = require("cors");
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:3001",
+};
+app.use(cors(corsOptions));
 
-app.listen(PORT, () => {
-  console.log("listening to port number ", PORT);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
 });
 db.mongoose
   .connect(db.url, {
@@ -33,4 +23,7 @@ db.mongoose
   .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
+  });
+  app.listen(PORT, () => {
+    console.log("listening to port number " + PORT);
   });
